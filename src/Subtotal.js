9fcdd63807b09f05aac1be888efc59be10fffcb6
reactbacksu <80665940+reactbacksu@users.noreply.github.com>
2021-03-15 22:43:54 +0900

    Update Subtotal.js

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -17,7 +17,7 @@ function Subtotal() {
                 renderText={(value) => (
                     <>
                         <p>
-                            총액 ( {basket?.length} items): <strong> {value} </strong>
+                            총액 ( {basket?.length} items): <strong> {value} 원 </strong>
                         </p>
                         <small className="subtotal_gift">
                             <input type="checkbox"/>체크박스입니다.
